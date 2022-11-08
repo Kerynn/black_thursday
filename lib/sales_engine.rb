4f4e147d46f81efdcbcebee9357d2c8d91d270fe
Ryan Canton <ryancanton999@gmail.com>
2022-11-07 19:01:42 -0700

    Refactor: Shorten conditional statement in sales engine initialize method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -25,42 +25,12 @@ class SalesEngine
               :customers
 
   def initialize(csv_hash)
-    if (csv_hash.has_key?(:items))
-      @items = create_item_repo(csv_hash[:items])
-    else
-      @items = nil
-    end
-
-    if (csv_hash.has_key?(:merchants))
-      @merchants = create_merchant_repo(csv_hash[:merchants])
-    else
-      @merchants = nil
-    end
-
-    if (csv_hash.has_key?(:invoices))
-      @invoices = create_invoice_repo(csv_hash[:invoices])
-    else
-      @invoices = nil
-    end
-
-    if (csv_hash.has_key?(:invoice_items))
-      @invoice_items = create_invoice_item_repo(csv_hash[:invoice_items])
-    else
-      @invoice_items = nil
-    end
-
-    if (csv_hash.has_key?(:transactions))
-      @transactions = create_transaction_repo(csv_hash[:transactions])
-    else
-      @transactions = nil
-    end
-
-    if (csv_hash.has_key?(:customers))
-      @customers = create_customer_repo(csv_hash[:customers])
-    else
-      @customers = nil
-    end
-
+    @items = create_item_repo(csv_hash[:items]) if (csv_hash.has_key?(:items))
+    @merchants = create_merchant_repo(csv_hash[:merchants]) if (csv_hash.has_key?(:merchants))
+    @invoices = create_invoice_repo(csv_hash[:invoices]) if (csv_hash.has_key?(:invoices))
+    @invoice_items = create_invoice_item_repo(csv_hash[:invoice_items]) if (csv_hash.has_key?(:invoice_items))
+    @transactions = create_transaction_repo(csv_hash[:transactions]) if (csv_hash.has_key?(:transactions))
+    @customers = create_customer_repo(csv_hash[:customers]) if (csv_hash.has_key?(:customers))
     @analyst = SalesAnalyst.new(@items, @merchants, @invoices, @invoice_items, @transactions)
   end
 
