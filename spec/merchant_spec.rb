318d48d47ce69bfff64813085872d2d541f288d1
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-08 19:44:59 -0700

    Updated tests, made quotes uniform

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -1,29 +1,20 @@
 require './spec/spec_helper'
 
 RSpec.describe Merchant do
-  describe '#initialize' do
-    before :each do
-      @m = Merchant.new({:id => 5, :name => "Turing School"})
+  let(:m){Merchant.new({:id => 5, 
+                        :name => 'Turing School'
+                        })}
+  
+  describe '#iteration 1' do
+    it '#initialize exists and has readable attributes' do
+      expect(m).to be_a(Merchant)
+      expect(m.id).to eq(5)
+      expect(m.name).to eq('Turing School')
     end
 
-    it 'exists' do
-
-      expect(@m).to be_a(Merchant)
-    end
-
-    it 'has readable attributes' do
-
-      expect(@m.id).to eq(5)
-      expect(@m.name).to eq("Turing School")
-    end
-  end
-
-  describe '#update_name' do
-    it 'updates a merchant objects name' do
-      @m = Merchant.new({:id => 5, :name => "Turing School"})
-      @m.update_name("Westpoint")
-
-      expect(@m.name).to eq("Westpoint")
+    it '#update_name updates a merchant objects name' do
+      m.update_name('Westpoint')
+      expect(m.name).to eq('Westpoint')
     end
   end
 end
