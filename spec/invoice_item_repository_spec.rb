7e562f38b81ae27f09bbdd0bed40b8054dd01cc6
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-08 23:23:11 -0700

    Updated formatting in invoice item repo spec

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -1,33 +1,30 @@
 require './spec/spec_helper'
 
 RSpec.describe InvoiceItemRepository do
-  let (:ii_1) {InvoiceItem.new({
-                :id => 6,
-                :item_id => 7,
-                :invoice_id => 10,
-                :quantity => 1,
-                :unit_price => BigDecimal(10.99, 4),
-                :created_at => Time.now,
-                :updated_at => Time.now
-              })}
-  let (:ii_2) {InvoiceItem.new({
-                :id => 4,
-                :item_id => 2,
-                :invoice_id => 10,
-                :quantity => 2,
-                :unit_price => BigDecimal(54.22, 4),
-                :created_at => Time.now,
-                :updated_at => Time.now
-              })}
-  let (:ii_3) {InvoiceItem.new({
-                :id => 8,
-                :item_id => 2,
-                :invoice_id => 4,
-                :quantity => 14,
-                :unit_price => BigDecimal(24.42, 4),
-                :created_at => Time.now,
-                :updated_at => Time.now
-              })}
+  let (:ii_1) {InvoiceItem.new({:id => 6,
+                                :item_id => 7,
+                                :invoice_id => 10,
+                                :quantity => 1,
+                                :unit_price => BigDecimal(10.99, 4),
+                                :created_at => Time.now,
+                                :updated_at => Time.now
+                              })}
+  let (:ii_2) {InvoiceItem.new({:id => 4,
+                                :item_id => 2,
+                                :invoice_id => 10,
+                                :quantity => 2,
+                                :unit_price => BigDecimal(54.22, 4),
+                                :created_at => Time.now,
+                                :updated_at => Time.now
+                              })}
+  let (:ii_3) {InvoiceItem.new({:id => 8,
+                                :item_id => 2,
+                                :invoice_id => 4,
+                                :quantity => 14,
+                                :unit_price => BigDecimal(24.42, 4),
+                                :created_at => Time.now,
+                                :updated_at => Time.now
+                              })}
   let (:invoice_items) {[ii_1, ii_2, ii_3]}
   let (:invoice_item_repository) {InvoiceItemRepository.new(invoice_items)}
 
