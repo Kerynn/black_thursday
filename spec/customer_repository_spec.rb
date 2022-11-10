e801df65f363f17cae4eb91d735e4a2f0b19ae3d
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-09 17:29:19 -0700

    Updated test formatting

--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -1,21 +1,24 @@
 require './spec/spec_helper'
 
 RSpec.describe CustomerRepository do
-  let (:customer_1){Customer.new({:id => 6,
+  let (:customer_1){Customer.new({
+                                  :id         => 6,
                                   :first_name => 'Joan',
-                                  :last_name => 'Clarke',
+                                  :last_name  => 'Clarke',
                                   :created_at => Time.now,
                                   :updated_at => Time.now
                                   })}
-  let (:customer_2){Customer.new({:id => 7,
+  let (:customer_2){Customer.new({
+                                  :id         => 7,
                                   :first_name => 'Richard',
-                                  :last_name => 'Hammilton',
+                                  :last_name  => 'Hammilton',
                                   :created_at => Time.now,
                                   :updated_at => Time.now
                                   })}
-  let (:customer_3){Customer.new({:id => 8,
+  let (:customer_3){Customer.new({
+                                  :id         => 8,
                                   :first_name => 'Mike',
-                                  :last_name => 'Lowry',
+                                  :last_name  => 'Lowry',
                                   :created_at => Time.now,
                                   :updated_at => Time.now
                                   })}
